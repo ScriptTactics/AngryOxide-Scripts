e75f4ee2e0882a84627f3d7128d6835b54b1af64
PWRxPSYCHO <pwrxpsycho@gmail.com>
2024-02-29 21:58:04 -0500

    updated install script to get latest version of AO

--- installations/getAngryOxide.sh
+++ installations/getAngryOxide.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 set -e
 
+sudo apt install jq
 
 CPU_ARCHTIECTURE=$(lscpu | grep Architecture | awk {'print $2'})
 
-wget https://github.com/Ragnt/AngryOxide/releases/download/v0.8.5/angryoxide-linux-${CPU_ARCHTIECTURE}-musl.tar.gz
+json=$(curl -s "https://api.github.com/repos/Ragnt/AngryOxide/releases")
+
+latest_tag=$(echo "$json" | jq -r '.[0].tag_name')
+
+version=$(echo "$latest_tag" | awk -F 'v' '{print $2}')
+
+wget https://github.com/Ragnt/AngryOxide/releases/download/${latest_tag}/angryoxide-linux-${CPU_ARCHTIECTURE}-musl.tar.gz
 
 tar -xvf angryoxide-linux-${CPU_ARCHTIECTURE}-musl.tar.gz
 
-sudo mv angryoxide /usr/local/bin/
 
-COMPETIONS=$(pkg-config --variable=completionsdir bash-completion)
+if [[ "$version" < "0.8.5" ]]; then
 
-sudo mv completions/angryoxide $COMPETIONS
+    sudo mv angryoxide /usr/local/bin/
+    COMPETIONS=$(pkg-config --variable=completionsdir bash-completion)
+    sudo mv completions/angryoxide $COMPETIONS
+    rm -rf completions/ angryoxide-linux-${CPU_ARCHTIECTURE}-musl.tar.gz
+else
 
-rm -rf completions/ angryoxide-linux-${CPU_ARCHTIECTURE}-musl.tar.gz
+    chmod +x install.sh
+    sudo ./install.sh install
+    rm -rf angryoxide completions/ angryoxide-linux-${CPU_ARCHTIECTURE}-musl.tar.gz
+fi
